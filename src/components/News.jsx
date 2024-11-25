206cc1a6a801e66c84e89f797390cd483df9ea1d
mohamednaeemm <naemm2048@gmail.com>
2024-11-25 11:57:29 +0200

    news-content

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,19 +2,83 @@ import React from 'react'
 import Weather from './Weather'
 import Calendar from './Calendar'
 import './News.css'
+import userImg from '../assets/images/user.jpg'
+import techImg from '../assets/images/tech.jpg'
+import sportsImg from '../assets/images/sports.jpg'
+import scienceImg from '../assets/images/science.jpg'
+import worldImg from '../assets/images/world.jpg'
+import healthImg from '../assets/images/health.jpg'
+import nationImg from '../assets/images/nation.jpg'
 
 const News = () => {
   return (
     <div className="news">
-      <header className="news-header">News Header</header>
-        <div className="news-content">
+      <header className="news-header">
+        <h1 className="logo">News & Blogs</h1>
+        <div className="search-bar">
+            <form action="">
+                <input type="text" placeholder='Search News...'/>
+                <button type="submit">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
+        </div>
+      </header>
+        <div className="news-content" >
             <div className="navbar">
-                <div className="user">User</div>
-                <nav className="categories">Categories</nav>
+                <div className="user">
+                    <img src={userImg} alt="User Image" />
+                    <p>Mohamed&apos;s Blog</p>
+                </div>
+                <nav className="categories">
+                    <h1 className="nav-heading">categories</h1>
+                    <div className="nav-links">
+                        <a href="" className="nav-link">General</a>
+                        <a href="" className="nav-link">World</a>
+                        <a href="" className="nav-link">Business</a>
+                        <a href="" className="nav-link">Technology</a>
+                        <a href="" className="nav-link">Entertainment</a>
+                        <a href="" className="nav-link">Sports</a>
+                        <a href="" className="nav-link">Science</a>
+                        <a href="" className="nav-link">Health</a>
+                        <a href="" className="nav-link">Nation</a>
+                        <a href="" className="nav-link">Bookmark <i className="fa-regular fa-bookmark"></i></a>
+                    </div>
+                </nav>
             </div>
             <div className="news-section">
-                <div className="headline">Headline</div>
-                <div className="news-grid">News Grid</div>
+                <div className="headline">
+                    <img src={techImg} alt="Headline Img" />
+                    <h2 className="headline-title">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, a.<i className="fa-regular fa-bookmark bookmark"></i>
+                    </h2>
+                </div>
+                <div className="news-grid">
+                    <div className="news-grid-item">
+                        <img src={techImg} alt="News image" />
+                        <h3>Lorem ipsum dolor sit amet.<i className="fa-regular fa-bookmark bookmark"></i></h3>
+                    </div>
+                    <div className="news-grid-item">
+                        <img src={sportsImg} alt="News image" />
+                        <h3>Lorem ipsum dolor sit amet.<i className="fa-regular fa-bookmark bookmark"></i></h3>
+                    </div>
+                    <div className="news-grid-item">
+                        <img src={scienceImg} alt="News image" />
+                        <h3>Lorem ipsum dolor sit amet.<i className="fa-regular fa-bookmark bookmark"></i></h3>
+                    </div>
+                    <div className="news-grid-item">
+                        <img src={worldImg} alt="News image" />
+                        <h3>Lorem ipsum dolor sit amet.<i className="fa-regular fa-bookmark bookmark"></i></h3>
+                    </div>
+                    <div className="news-grid-item">
+                        <img src={healthImg} alt="News image" />
+                        <h3>Lorem ipsum dolor sit amet.<i className="fa-regular fa-bookmark bookmark"></i></h3>
+                    </div>
+                    <div className="news-grid-item">
+                        <img src={nationImg} alt="News image" />
+                        <h3>Lorem ipsum dolor sit amet.<i className="fa-regular fa-bookmark bookmark"></i></h3>
+                    </div>
+                </div>
             </div>
             <div className="my-blogs">My Blogs</div>
             <div className="weather-calendar">
